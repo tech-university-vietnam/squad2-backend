77f09b646761a08fcc0acdd31d45eb52e184208f
lamngo-mck <lam_ngo_phan_nhat@mckinsey.com>
2022-08-17 23:18:35 +0700

    feat: connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './modules/users/user.module';
+import { UserController } from './modules/users/user.controller';
+import { UserService } from './modules/users/user.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule],
+  controllers: [UserController],
+  exports: [UserService],
+  providers: [UserService],
 })
 export class AppModule {}
---
+++ src/modules/users/user.entity.ts
@@ -0,0 +1,25 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class UserEntity extends BaseEntity{
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  avatar: string;
+
+  @Column()
+  gender: string;
+}
