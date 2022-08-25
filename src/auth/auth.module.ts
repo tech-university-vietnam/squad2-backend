71ec14d1f368e52be740b613966d7a29dd029eb4
lamngo-mck <lam_ngo_phan_nhat@mckinsey.com>
2022-08-25 09:29:07 +0700

    fix: import User repository

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersRepository } from '../users/users.repository';
 import AuthService from './auth.service';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [AuthService],
   exports: [AuthService],
 })
