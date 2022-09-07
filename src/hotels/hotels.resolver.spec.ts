637cc12d587a5171f292693411c8eac69fbb38ff
Huy Luu <luuhoanganhhuy2012@gmail.com>
2022-09-07 16:32:34 +0700

    test: unit test for hotel service

--- src/hotels/hotels.resolver.spec.ts
+++ src/hotels/hotels.resolver.spec.ts
@@ -1,13 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HotelsResolver } from './hotels.resolver';
 import { HotelsService } from './hotels.service';
+import { CreateHotelInput } from './dto/create-hotel.input';
+import { UpdateHotelInput } from './dto/update-hotel.input';
+import { ListHotelsInput } from './dto/list-hotel.input';
 
 describe('HotelsResolver', () => {
   let resolver: HotelsResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HotelsResolver, HotelsService],
+      providers: [
+        HotelsResolver,
+        HotelsService,
+        {
+          provide: 'IHotelsRepository',
+          useValue: {
+            Create: jest.fn(),
+            List: jest.fn(),
+            GetByID: jest.fn(),
+            Update: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     resolver = module.get<HotelsResolver>(HotelsResolver);
@@ -15,5 +30,13 @@ describe('HotelsResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+  it('create a hotel resolver', () => {
+    const dto = new CreateHotelInput();
+    expect(resolver.create(dto)).not.toEqual(null);
+  });
+  it('update a hotel resolver', () => {
+    const dto = new UpdateHotelInput();
+    expect(resolver.update(dto)).not.toEqual(null);
   });
 });
