83d4aa5e260550d97ef01b23f0b8b8e7085ef351
lamngo-mck <lam_ngo_phan_nhat@mckinsey.com>
2022-09-08 13:36:36 +0700

    feat: inject userRepository to auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import AuthService from './auth.service';
 import { User } from '../users/entities/user.entity';
+import { UsersRepository } from '../users/users.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: 'IUsersRepository',
+      useClass: UsersRepository,
+    },
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
