9856f87e8abb0c1deb1101ea7ee7afe201581425
Huy Luu <luuhoanganhhuy2012@gmail.com>
2022-09-09 14:52:43 +0700

    test: 85% coverage for unit test

--- src/bookings/bookings.service.spec.ts
+++ src/bookings/bookings.service.spec.ts
@@ -1,6 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookingsService } from './bookings.service';
 
+const bookingArray = [
+  {
+    id: 1,
+    checkIn: '2021-01-01',
+    checkOut: '2021-01-02',
+    totalPrice: 100,
+    guests: 2,
+    createdAt: new Date(),
+    hotelId: 1,
+    userId: 1,
+  },
+];
+
 describe('BookingsService', () => {
   let service: BookingsService;
 
@@ -11,9 +24,9 @@ describe('BookingsService', () => {
         {
           provide: 'IBookingsRepository',
           useValue: {
-            Create: jest.fn().mockReturnValue({}),
-            List: jest.fn().mockResolvedValue([]),
-            GetByID: jest.fn().mockResolvedValue({}),
+            Create: jest.fn().mockReturnValue(bookingArray[0]),
+            List: jest.fn().mockResolvedValue(bookingArray),
+            GetByID: jest.fn().mockResolvedValue(bookingArray[0]),
             Update: jest.fn().mockResolvedValue(true),
           },
         },
@@ -37,5 +50,30 @@ describe('BookingsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('should return an array of bookings', async () => {
+    const findAllSpy = jest.spyOn(service, 'findAll');
+    const bookings = await service.findAll();
+    expect(findAllSpy).toHaveBeenCalled();
+    expect(bookings).toEqual(bookingArray);
+  });
+  it('should return a booking', async () => {
+    const findOneSpy = jest.spyOn(service, 'findOne');
+    const booking = await service.findOne(1);
+    expect(findOneSpy).toHaveBeenCalled();
+    expect(booking).toEqual(bookingArray[0]);
+  });
+  it('should create a booking', async () => {
+    const createSpy = jest.spyOn(service, 'create');
+    await expect(service.create(bookingArray[0])).resolves.toEqual(
+      bookingArray[0],
+    );
+    expect(createSpy).toHaveBeenCalled();
+  });
+
+  it('should update a booking', async () => {
+    const updateSpy = jest.spyOn(service, 'update');
+    await expect(service.update(1, bookingArray[0])).resolves.toEqual(true);
+    expect(updateSpy).toHaveBeenCalled();
   });
 });
